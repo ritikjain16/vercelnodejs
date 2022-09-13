594c134a403ec1673e520eab2a643638c9e17c4b
Ritik Jain <ritik9628@gmail.com>
2022-09-13 18:32:06 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = process.env.PORT || 9001;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Ritik</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Listening to ${port}`);
+});
